5eecf050faf0279c23d0591baf1708df1af83228
lcdunne <56237933+lcdunne@users.noreply.github.com>
2022-05-18 20:14:20 +0100

    Update confint_correct.py

--- confint_correct.py
+++ confint_correct.py
@@ -1,5 +1,5 @@
 def _make_correction(data, id_var, within, y):
     # Correct data for within-subjects so that CIs are for within-subs diffs.
-    X = data.pivot(index=id_var, columns=within, values=dv)
+    X = data.pivot(index=id_var, columns=within, values=y)
     Y = (X.T + X.mean().mean() - X.mean(axis=1)).T
     return Y.reset_index().melt(id_vars=id_var, value_name=y)
